7f6abb69a6a0235d20b73eba57fdedb181390eb0
Ilham Dean Abdillah <deanabdillah@gmail.com>
2025-03-07 09:55:38 +0700

    update greeting message in index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.APP_PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello CORE!");
+    res.send("Hello CORE! cuyy");
 });
 
 app.listen(port, () => {
